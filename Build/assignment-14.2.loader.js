b6ab48ac5b7d22a305a5caaa95ee3f0bf8917e4c
Matthew <unconfigured@null.spigotmc.org>
2026-01-22 19:58:15 +0000

    Adding a Menu Screen

--- Build/assignment-14.2.loader.js
+++ Build/assignment-14.2.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 45620093,
+    wasmFileSize: 46839485,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
